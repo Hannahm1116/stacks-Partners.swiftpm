1dd82a7ddcc314a2f787c9ae3a60a5f7b54823fc
Joshua Ilagan <Jilagan6194@stu.d214.org>
2023-09-12 10:40:49 -0500

    I made it so you can pick which view you see.

--- ContentView.swift
+++ ContentView.swift
@@ -1,9 +1,32 @@
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            
-        }
+struct ExampleView: View
+{
+  @State private var selectedView = 0
+  private let pickerOptions = ["Calculator", "Shapes"]
+  
+  var body: some View {
+    VStack {
+      
+      Divider()
+       
+      Picker(selection: $selectedView, label: Text("")) {
+        ForEach(0..<pickerOptions.count) {
+          Text(self.pickerOptions[$0])
+        } //: ForEach
+      } //: Picker
+      .pickerStyle(SegmentedPickerStyle())
+      .padding()
+      
+      if selectedView == 0
+      {
+          
+          CalculatorView()
+      }
+      else if selectedView == 1
+      {
+          ShapesView()
+      }
     }
+  }
 }
